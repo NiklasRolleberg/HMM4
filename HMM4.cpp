0fe575fb91077e316f9e8888dbaaa8f9b652b7f7
Niklas <mailaInte@a.b>
2014-10-09 19:51:04 +0200

    mycket nytt

--- HMM4.cpp
+++ HMM4.cpp
@@ -4,270 +4,270 @@
 #include <cmath>
 #include <cstdlib>
 #include <sstream>
+#include <limits>
 
-struct matrix
-{
-	int row;
-	int col;
-	double** m;
-
-	//constructor
-	matrix(double** mat, int rows, int cols)
-	{
-		row = rows;
-		col = cols;
-		m = mat;
-	}
-
-	//constructor 2
-	matrix(std::string in)
-	{
-		std::istringstream iss;
-		iss.str(in);
-		iss >> row >> col;
-
-        m = initialize(row,col);
-
-		for(int i=0;i<row;++i)
-		{
-			for(int j=0;j<col;++j)
-			iss >> m[i][j];
-		}
-	}
-
-    //creted the 2d matrix
-	static double** initialize(int rows, int cols)
-	{
-	    double** temp;
-	    temp = (double**)calloc(rows , sizeof(double *));
-		for(int i=0 ; i< rows ; ++i)
-			temp[i] = (double*)calloc(cols , sizeof(double));
-        return temp;
-	}
-
-	double get(int r, int c)
-	{
-		return m[r][c];
-	}
-
-	void print()
-	{
-		for (int i=0;i<row;++i)
-		{
-			for(int j=0;j<col;++j)
-				std::cout << m[i][j] <<" ";
-			std::cout << std::endl;
-		}
-	}
-
-	void writeMatrix()
-	{
-		std::cout << row << " " << col;
-		for(int i=0;i<row;++i) {
-			for(int j=0; j<col;++j)
-				std::cout << " " << m[i][j];
-		}
-	}
-
-	matrix transpose()
-	{
-		int i,j;
-		double** temp = initialize(col,row);
-		for(i=0;i<row;++i)
-        {
-            for(j=0;j<col;++j)
-                temp[j][i] = m[i][j];
-        }
-        return matrix(temp,col,row);
-	}
-
-	matrix getCol(int index)
-	{
-		int newRows = row;
-		int newCols = 1;
-
-		double** temp = initialize(newRows,newCols);
-
-		for(int i=0;i<newRows;++i)
-			temp[i][0] = m[i][index];
-		return matrix(temp,newRows,newCols);
-	}
-
-	matrix getRow(int index)
-	{
-		int newRows = 1;
-		int newCols = col;
-
-		double** temp = initialize(newRows,newCols);
-
-		for(int i=0;i<newCols;++i)
-			temp[0][i] = m[index][i];
-		return matrix(temp,newRows,newCols);
-	}
-
-	matrix operator*(matrix H)
-	{
-		int newRows = row;
-		int newCols = H.col;
-
-		if(col != H.row)
-			std::cerr << "Fel. matrix 1: " << row << " " << col << ", matrix 2: " << H.row << " " << H.col << std::endl;
-
-		double** temp;
-		temp = initialize(newRows,newCols);
-
-		//RÄKNA!
-		double t;
-		int i,j,k;
-		for (i=0;i<newRows;++i)
-		{
-			for(j=0;j<newCols;++j)
-			{
-				t = 0;
-				//row in first matrix X col in second matrix;
-				for(k=0;k<col;++k)
-					t+= (m[i][k] * H.get(k,j));
-				temp[i][j] = t;
-			}
-		}
-		return matrix(temp,newRows,newCols);
-	}
-
-	matrix operator&(matrix H) // element multiplication
-	{
-		int newRows = row;
-		int newCols = col;
-
-		if(row!=H.row || col != H.col)
-			std::cout << "FEL " << row << " " << H.row << " , " << col <<" " <<H.col <<std::endl;
-
-		double** temp = initialize(newRows,newCols);
-
-		//RÄKNA!
-		int i,j;
-		for (i=0;i<newRows;++i)
-		{
-			for(j=0;j<newCols;++j)
-				temp[i][j] = m[i][j]*H.get(i,j);
-		}
-		return matrix(temp,newRows,newCols);
-	}
-
+struct data{
+int row;
+int col;
+double** matris;
 };
+
+data init(std::string in);
+double** initialize(int rows, int cols);
+
+data init(std::string in)
+{
+	data temp;
+
+	std::istringstream iss;
+	iss.str(in);
+	iss >> temp.row >> temp.col;
+	
+	temp.matris = initialize(temp.row,temp.col);
+
+	for(int i=0;i<temp.row;++i)
+	{
+		for(int j=0;j<temp.col;++j)
+		iss >> temp.matris[i][j];
+	}
+	return temp;
+}
+
+double** initialize(int rows, int cols)
+{
+	double** temp;
+	temp = (double**)calloc(rows , sizeof(double *));
+	for(int i=0 ; i< rows ; ++i)
+		temp[i] = (double*)calloc(cols , sizeof(double));
+	return temp;
+}
 
 int main(int argc, char **argv)
 {
+	int T;
+	int M;
+	int N;
+	
+	int MaxItter = 100;
+	int itter = 0;
+	double OldLogProb = -std::numeric_limits<double>::max();
+	
 	// Read the file
 	std::vector<std::string> board;
 	for (std::string line; std::getline(std::cin, line);)
 		board.push_back(line);
 
-	matrix A = matrix(board[0]);
-	matrix B = matrix(board[1]);
-	matrix q = matrix(board[2]);
+	//initialize
+	data temp = init(board[0]);
+	double** A = temp.matris;
+	N = temp.row;
+	
+	temp = init(board[1]);
+	double** B = temp.matris;
+	M = temp.col;
+	
+	temp = init(board[2]);
+	double* q = temp.matris[0];
+	
 	std::vector<int> seq;
-
-	//Felkoll
-
-    /*
-    std::cout << "\nA" <<std::endl;
-    A.print();
-    std::cout << "\nB" <<std::endl;
-    B.print();
-    std::cout << "\nq" <<std::endl;
-    q.print();
-
-	//matrix X = A*B*A*B*A*B*A*B*(q*A).transpose(); //ger rätt svar
-    //X.print();
-    */
-
-	//find out the statesequence
-	int Nstates;
+	
 	int index;
 	std::istringstream iss;
 	iss.str(board[3]);
-	iss >> Nstates;
-	for(int i=0;i<Nstates;++i)
+	iss >> T;
+	for(int i=0;i<T;++i)
 	{
 		iss >> index;
 		seq.push_back(index);
 	}
 	
+	//variabler som går att återanvända
+	double* c = (double*) calloc(T,sizeof(double)); //c[time]
+	double** alpha = initialize(T, N);  //alpha[time][index]
+	double** beta = initialize(T, N);  //beta[time][index]
+	double** Gamma = initialize(T,N);
+	double*** diGamma = (double***)calloc(T,sizeof(double**)); //diGamma[time][index1][index2]
+	for(int t=0;t<T;++t)
+		diGamma[t] = initialize(N,N);
 	
-	//forward
-	
-	//matrix with all alpha values
-	matrix alpha = matrix(A.initialize(B.row,Nstates),B.row,Nstates);
-	
-	matrix L = q&B.getCol(seq[0]).transpose();
-	
-	for(int j=0;j<B.row;++j)
-			alpha.m[j][0] = L.get(0,j);
-			
-	for(int i=1;i<Nstates;++i)
+	/*Felkoll
+	std::cout << "A" << std::endl;
+	for(int i=0;i<N;++i)
 	{
-		L = (L*A)&(B.getCol(seq[i]).transpose());
-		
-		for(int j=0;j<B.row;++j)
-			alpha.m[j][i] = L.get(0,j);
+		for(int j=0;j<N;++j)
+			std::cout << A[i][j] << " ";
+		std::cout<<std::endl;
 	}
-
-	//sum the elements in L
-	double sumA = 0;
-	for(int i=0;i<L.col;++i)
-            sumA += L.get(0,i);
-	std::cout << "Forward: " << sumA << std::endl;
 	
-	//Backward
-	
-	//matrx with all beta values
-	matrix beta = matrix(A.initialize(A.row,Nstates),A.row,Nstates);
-	
-	double** temp = A.initialize(1,A.col);
-	for(int i=0;i<A.col;++i)
-		temp[0][i] = 1.0;
-		
-	matrix b = matrix(temp,1,A.col);
-	for(int j=0;j<B.row;++j)
-			beta.m[j][Nstates-1] = b.get(0,j);
-			
-	for(int k=Nstates-2;k>=0;--k)
+	std::cout << "\nB" << std::endl;
+	for(int i=0;i<N;++i)
 	{
-		//b = (A*B.getCol(seq[k])).transpose()&b; //antagligen fel
+		for(int j=0;j<M;++j)
+			std::cout << B[i][j] << " ";
+		std::cout<<std::endl;
+	}
+	
+	std::cout << "\nq" << std::endl;
+	for(int i=0;i<N;++i)
+		std::cout << q[i] << " ";
+	*/
+	
+	bool GO = true;
+	while((itter < MaxItter) && GO)
+	{
 		
-		for(int i=0;i<A.col;++i)
+		/**----------Alpha-pass------------------------*/
+		
+		//alpha-0
+		c[0] = 0;
+		
+		for(int i=0;i<N;++i)
 		{
-			double temp = 0;
-			for(int j=0;j<A.row;++j)
-				temp += A.get(i,j) * B.get(j,seq[k]) * b.get(0,j);
-			
-			b.m[0][i] = temp;
+			alpha[0][i] = q[i]*B[i][seq[0]];
+			c[0] += alpha[0][i];
+		}
+		//scale
+		c[0] = 1.0/c[0];
+		for(int i=0;i<N;++i)
+		{
+			alpha[0][i] *= c[0];
 		}
 		
-		for(int j=0;j<B.row;++j)
-			beta.m[j][k] = b.get(0,j);		
+		//alpha-t
+		for(int t=1;t<T;++t)
+		{
+			c[t] = 0;
+			for(int i=0;i<N;++i)
+			{
+				alpha[t][i] = 0;
+				for(int j=0;j<N;++j)
+				{
+					alpha[t][i] += alpha[t-1][j]*A[i][j];
+				}
+				alpha[t][i] *= B[i][seq[t]];
+				c[t] += alpha[t][i];
+			}
+			
+			//scale
+			c[t] = 1.0/c[t];
+			for(int i=0;i<N;++i)
+			{
+				alpha[t][i] *= c[t];
+			}
+		}
+		
+		/*for(int i=0;i<N;++i)
+			std::cout << alpha[T-1][i] << " ";*/
+			
+		/**----------Beta-pass------------------------*/
+		
+		//beta T-1
+		for(int i=0;i<N;++i)
+			beta[T-1][i] = c[T-1];
+			
+		for(int t=T-2; t>=0;--t)
+		{
+			for(int i=0;i<N;++i)
+			{
+				beta[t][i] = 0;
+				for(int j=0;j<N;++j)
+				{
+					beta[t][i] += A[i][j] * B[j][seq[t]] * beta[t+1][j];
+				}
+				//scale
+				beta[t][i] *= c[t];
+			}
+		}
+		
+		
+		/**----------diGamma & Gamma----------------*/
+		
+		for(int t=0;t<(T-1);++t)
+		{
+			double denom = 0;
+			for(int i=0;i<N;++i)
+				for(int j=0;j<N;++j)
+					denom += alpha[t][i] * A[i][j] * B[j][seq[t+1]] * beta[t+1][j];
+			
+			for(int i=0;i<N;++i)
+			{
+				Gamma[t][i] = 0;
+				for(int j=0;j<N;++j)
+				{
+					diGamma[t][i][j] = (alpha[t][i] * A[i][j] * B[j][seq[t+1]] * beta[t+1][j]) / denom;
+					Gamma[t][i] += diGamma[t][i][j];
+				}
+			}
+	
+		}
+		
+		/**----------Estimate A,b,q------------------*/
+		
+		/*q*/
+		for(int i=0;i<N;++i)
+			q[i] = Gamma[0][i];
+		
+		/*A*/
+		for(int i=0;i<N;++i)
+		{
+			for(int j=0;j<N;++j)
+			{
+				double numer = 0;
+				double denom = 0;
+				for(int t=0;t<(T-1);++t)
+				{
+					numer += diGamma[t][i][j];
+					denom += Gamma[t][i];
+				}
+			A[i][j] = numer/denom;
+			}
+		}
+
+		/*B*/
+		for(int i=0;i<N;++i)
+		{
+			for(int j=0;j<M;++j)
+			{
+				double numer = 0;
+				double denom = 0;
+				for(int t=0;t<(T-1);++t)
+				{
+					if(seq[t] == j)
+						numer += Gamma[t][i];
+					denom += Gamma[t][i];
+				}
+			B[i][j] = numer/denom;
+			}
+		}
+		
+		double logProb = 0;
+		for(int t=0;t<T;++t)
+			logProb += log(c[t]);
+		logProb *= -1;
+		
+		if(logProb < OldLogProb)
+		{
+			GO = false;
+			break;
+		}
+		
+		OldLogProb = logProb;
+		itter++;
 	}
-	std::cout << "Backward: ";
-	b.print();
 	
-	std::cout << "\nresten\n" <<std::endl;
-	alpha.print();
-	std::cout <<"\n"<<std::endl;
-	beta.print();
+	std::cout << "Iterations " << itter << std::endl;
 	
-	//Di-gamma
-	matrix diGamma(A.initialize(A.row,A.col),A.row,A.col);
+	//Print matrix
+	std::cout << N << " " << N;
+	for(int i=0;i<N;++i)
+		for(int j=0;j<N;++j)
+			std::cout << " " << A[i][j];
 	
-	int t = 0;
+	std::cout << "\n" << N << " " << M;
+	for(int i=0;i<N;++i)
+		for(int j=0;j<M;++j)
+			std::cout << " " << B[i][j];
+	std::cout << std::endl;
 	
-	for(int i=0;i<A.row;++i)
-	{
-		for(int j=0;j<A.col;++j)
-			diGamma.m[i][j] = (alpha.get(i,t) * A.get(i,j) * B.get(j,t+1))/sumA;
-	}
-	
-	std::cout << "\ndi-gamma\n" << std::endl;
-	diGamma.print();
 	return 0;
 }
-
